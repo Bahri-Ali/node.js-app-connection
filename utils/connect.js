ff35a53dced156d7373894272ee99f305ee6f318
Bahri-Ali <152076979+Bahri-Ali@users.noreply.github.com>
2024-10-20 12:02:01 +0100

    Update connect.js

--- utils/connect.js
+++ utils/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectDataBasse = ()=>{
     try{
-        mongoose.connect('mongodb+srv://alibahridev:TW1DgAgJGW77a7VI@cluster0.zo67f.mongodb.net/')
+        mongoose.connect('mongodb+srv://alibahridev:.mongodb.net/')
         .then(()=>{console.log('we can connect')})
     }catch{
         console.log('problem here of login')
